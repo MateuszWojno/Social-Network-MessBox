0d568fe95cccd2957be4a71135941da29212b5f5
MateuszWojno <mateusz.wojno9922@gmail.com>
2022-11-26 09:42:26 +0100

    Move HtmlContent from test/test.php to test/HtmlContent.php

--- test/test.php
+++ test/test.php
@@ -1,49 +1,5 @@
 <?php
-
-class HtmlContent
-{
-    private DOMXpath $xPath;
-
-    public function __construct(string $html)
-    {
-        $this->xPath = new \DOMXpath($this->document($html));
-    }
-
-    private function document(string $html): DomDocument
-    {
-        $document = new \DomDocument();
-        @$document->loadHTML($html);
-        return $document;
-    }
-
-    public function element(string $xPath): string
-    {
-        $elements = $this->elements($xPath);
-        if (empty($elements)) {
-            throw new Exception("Failed to find element matching '$xPath'");
-        }
-        $count = count($elements);
-        if ($count === 1) {
-            return $elements[0];
-        }
-        throw new Exception("Failed to match selector '$xPath' uniquely, $count elements matched");
-    }
-
-    public function elements(string $xPath): array
-    {
-        $elements = $this->xPath->query($xPath);
-        if ($elements === false) {
-            throw new Exception("Malformed xPath selector: '$xPath'");
-        }
-        $results = [];
-        foreach ($elements as $element) {
-            foreach ($element->childNodes as $node) {
-                $results[] = $node->nodeValue;
-            }
-        }
-        return $results;
-    }
-}
+require "HtmlContent.php";
 
 function renderedPage(string $entryPoint): string
 {
