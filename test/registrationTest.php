b97995ca9cf2337e41f65313b6ca7392f23af500
MateuszWojno <mateusz.wojno9922@gmail.com>
2022-11-30 23:08:19 +0100

    Add view tests in test/registrationTest.php

--- test/registrationTest.php
+++ test/registrationTest.php
@@ -15,14 +15,14 @@ function openPage(string $entryPoint): HtmlContent
 
 $registrationRequest = [
     'login'          => 'Jan',
-    'password'       => 'aa',
-    'passwordRepeat' => 'aaaaa',
+    'password'       => 'aaaaaa',
+    'passwordRepeat' => 'aaaaaa',
     'firstName'      => 'Jan',
     'lastName'       => 'Mic',
-    'email'          => 'mail@mail',
+    'email'          => 'email@email.com',
     'phoneNumber'    => '000000000',
-    'birthDate'      => '01.01.2011',
-    'gender'         => 'mężczyzna',
+    'birthDate'      => '22.10.2012',
+    'gender'         => 'kobieta',
     'avatar'         => 'avatar.jpg',
     'signUp'         => 1
 ];
@@ -42,6 +42,96 @@ assertEquals("Zarejestruj", $content);
 
 $content = $html->elements("//span[@class='error']");
 assertArrayEquals(['', '', '', '', '', '', '', '', '', ''], $content);
+
+$content = $html->elements("//span[@class='error']");
+assertArrayEquals(['Podaj login'], $content);
+
+$content = $html->elements("//span[@class='error']");
+assertArrayEquals(['Podany login już istnieje'], $content);
+
+$content = $html->elements("//span[@class='error']");
+assertArrayEquals(['Podaj hasło', 'Nieprawidłowe hasło'], $content);
+
+$content = $html->elements("//span[@class='error']");
+assertArrayEquals(['Niepoprawne hasło'], $content);
+
+$content = $html->elements("//span[@class='error']");
+assertArrayEquals(['Niepoprawne hasło'], $content);
+
+$content = $html->elements("//span[@class='error']");
+assertArrayEquals(['Niepoprawne hasło'], $content);
+
+$content = $html->elements("//span[@class='error']");
+assertArrayEquals(['Niepoprawne hasło'], $content);
+
+$content = $html->elements("//span[@class='error']");
+assertArrayEquals(['Hasła nie są takie same'], $content);
+
+$content = $html->elements("//span[@class='error']");
+assertArrayEquals(['Podaj imię'], $content);
+
+$content = $html->elements("//span[@class='error']");
+assertArrayEquals(['Niepoprawne imię'], $content);
+
+$content = $html->elements("//span[@class='error']");
+assertArrayEquals(['Niepoprawne imię'], $content);
+
+$content = $html->elements("//span[@class='error']");
+assertArrayEquals(['Niepoprawne imię'], $content);
+
+$content = $html->elements("//span[@class='error']");
+assertArrayEquals(['Niepoprawne imię'], $content);
+
+$content = $html->elements("//span[@class='error']");
+assertArrayEquals(['Podaj nazwisko'], $content);
+
+$content = $html->elements("//span[@class='error']");
+assertArrayEquals(['Niepoprawne nazwisko'], $content);
+
+$content = $html->elements("//span[@class='error']");
+assertArrayEquals(['Niepoprawne nazwisko'], $content);
+
+$content = $html->elements("//span[@class='error']");
+assertArrayEquals(['Niepoprawne nazwisko'], $content);
+
+$content = $html->elements("//span[@class='error']");
+assertArrayEquals(['Niepoprawne nazwisko'], $content);
+
+$content = $html->elements("//span[@class='error']");
+assertArrayEquals(['Podaj email'], $content);
+
+$content = $html->elements("//span[@class='error']");
+assertArrayEquals(['Niepoprawny email'], $content);
+
+$content = $html->elements("//span[@class='error']");
+assertArrayEquals(['Podany email już istnieje'], $content);
+
+$content = $html->elements("//span[@class='error']");
+assertArrayEquals(['Podaj numer telefonu'], $content);
+
+$content = $html->elements("//span[@class='error']");
+assertArrayEquals(['Niepoprawny numer'], $content);
+
+$content = $html->elements("//span[@class='error']");
+assertArrayEquals(['Niepoprawny format'], $content);
+
+$content = $html->elements("//span[@class='error']");
+assertArrayEquals(['Niepoprawny format'], $content);
+
+$content = $html->elements("//span[@class='error']");
+assertArrayEquals(['Podany numer już istnieje'], $content);
+
+$content = $html->elements("//span[@class='error']");
+assertArrayEquals(['Wybierz datę urodzenia'], $content);
+
+$content = $html->elements("//span[@class='error']");
+assertArrayEquals(['Nie masz ukończonych 10 lat'], $content);
+
+$content = $html->elements("//span[@class='error']");
+assertArrayEquals(['Wybierz płeć'], $content);
+
+$content = $html->elements("//span[@class='error']");
+assertArrayEquals(['Nieprawidłowe rozszerzenie pliku'], $content);
 
 function assertEquals(string $expected, string $content): void
 {
